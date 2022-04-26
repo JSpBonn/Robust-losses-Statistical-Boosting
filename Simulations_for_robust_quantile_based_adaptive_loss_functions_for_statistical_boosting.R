f2bcaacff4c352056e269f12889b4b8752378034
JSpBonn <64110672+JSpBonn@users.noreply.github.com>
2022-04-26 19:38:58 +0200

    Update Simulations_for_robust_quantile_based_adaptive_loss_functions_for_statistical_boosting.R

--- Simulations_for_robust_quantile_based_adaptive_loss_functions_for_statistical_boosting.R
+++ Simulations_for_robust_quantile_based_adaptive_loss_functions_for_statistical_boosting.R
@@ -6,123 +6,9 @@ library(mvtnorm)
 library(mboost)
 library(parallel)
 
+source(Robust_quantile_based_adaptive_loss_functions_for_statistical_boosting.R) # loading the adaptive loss functions
 
-# M_Bisquare
-M_Bisquare <- function (tau = 0.99 , k=NULL, pi=NULL){ 
-  k_to_tau <- function(k){
-    return((pnorm(k)-pnorm(-k))) # =1-2*pnorm(-k)
-  }
-  
-  if( !is.null(tau)){
-    if((0<=tau)*(tau<=1)){message(paste("using tau = ",tau,sep=""))
-      tau_to_k <- function(tau){
-        to_minimize <- function(k){return(abs(pnorm(k)-pnorm(-k)-tau))}
-        return(optimize(f=to_minimize,interval = c(0.1,8.0),maximum=FALSE)$minimum)
-      }
-      k=tau_to_k(tau)
-      message(paste("corresponding to k = ",k," for standard gaussian distribution",sep=""))
-      pi=1-tau
-      message(paste("corresponding to pi = ", pi,sep=""))
-    } else {stop("tau is a quantile, choose value in [0,1]")}
-  } else{
-    if(!is.null(k)){
-      if(k>0){message(paste("using k = ",k," for standard gaussian distribution",sep=""))
-        tau=k_to_tau(k)
-        pi=1-tau
-        message(paste("corresponding to pi = ",pi,sep=""))
-        message(paste("transferred to tau = ",tau, sep=""))
-      } else {stop("k has to be a positive value")}
-    } else{
-      if(!is.null(pi)){
-        if((0<=pi)*(pi<=1)){  message(paste("using pi = ",pi,sep=""))
-          tau=1-pi
-          tau_to_k <- function(tau){
-            to_minimize <- function(k){return(abs(pnorm(k)-pnorm(-k)-tau))}
-            return(optimize(f=to_minimize,interval = c(0.1,8.0),maximum=FALSE)$minimum)
-          }
-          k=tau_to_k(tau)
-          message(paste("corresponding to k = ", k," for standard gaussian distribution",sep=""))
-          message(paste("transferred to tau = ",tau,sep=""))
-        } else {stop("pi is an amount, choose value in [0,1]")}
-      } 
-    }
-  }
-  
-  Family(ngradient = function(y, f,w = rep(1, length(y))) {
-    d <- quantile(abs(y - f)[rep(1:length(y), w)],probs=tau)
-    (abs(y-f)<=d)*(y-f)*(1-((y-f)/d)^2)^2}, 
-    loss = function(y, f,w = rep(1, length(y))) {
-      d <- quantile(abs(y - f)[rep(1:length(y), w)],probs=tau)
-      (abs(y-f)<=d)*d^2/6*(1-(1-((y-f)/d)^2)^3)+(abs(y-f)>d)/6*1},
-    offset = function(y,w=rep(1, length(y))){
-      median(y[rep(1:length(y), w)])}
-  )
-}
-
-
-
-M_Quant_Huber <- function(tau= 0.8213748 , k=NULL , pi=NULL , q = 0.5){
-  k_to_tau <- function(k){
-    return((pnorm(k)-pnorm(-k))) # =1-2*pnorm(-k)
-  }
-  
-  k_to_pi <- function(k){
-    return((1/(1-2*pnorm(-k)+2*dnorm(k)/k)-1)*(-1))
-  }
-  if(tau!=0.8213748){
-    if((0<=tau)*(tau<=1)){message(paste("using tau = ",tau,sep=""))
-      tau_to_k <- function(tau){
-        to_minimize2 <- function(k){ return(abs(pnorm(k)-pnorm(-k)-tau))}
-        return(optimize(f=to_minimize2,interval = c(0.1,8.0),maximum=FALSE)$minimum)
-      }
-      k=tau_to_k(tau)
-      message(paste("corresponding to k = ",k," for standard gaussian distribution, recommanded is k in [1,2] for robust results",sep=""))
-      pi=k_to_pi(k)
-      message(paste("corresponding to pi = ", pi,sep=""))
-    } else {stop("tau is a quantile, choose value in [0,1]")}
-  } else{
-    if(!is.null(k)){
-      if(k>0){message(paste("using k = ",k," for standard gaussian distribution, recommanded is k in [1,2] for robust results",sep=""))
-        pi=k_to_pi(k)
-        tau=k_to_tau(k)
-        message(paste("corresponding to pi = ",pi,sep=""))
-        message(paste("transferred to tau = ",tau, sep=""))
-      } else {stop("k has to be a positive value")}
-    } else{
-      if(!is.null(pi)){
-        if((0<=pi)*(pi<=1)){  message(paste("using pi = ",pi,sep=""))
-          pi_to_k <- function(pi){
-            to_minimize<- function(k){return(abs((1/(1-2*pnorm(-k)+2*dnorm(k)/k)-1)*(-1)-pi))}
-            return(optimize(f=to_minimize,interval = c(0.01,8.00),maximum=FALSE)$minimum)
-          }
-          k=pi_to_k(pi)
-          tau=k_to_tau(k)
-          message(paste("corresponding to k = ", k," for standard gaussian distribution, recommanded is k in [1,2] for robust results",sep=""))
-          message(paste("transferred to tau = ",tau,sep=""))
-          
-        } else {stop("pi is an amount, choose value in [0,1]")}
-      } else {tau=0.8213748
-      message(paste("using tau = ",tau,sep=""))
-      message("corresponding to k = 1.345")
-      message("corresponding to pi = 0.05791489")}
-    }
-  }
-  
-  
-  Family(
-    loss=function(y,f, w = rep(1, length(y))){
-      d_1 <- quantile(abs(y-f)[rep(1:length(y), w)], probs = tau)
-      ifelse( y-f < -d_1, -2*(1 - q)*d_1*(y - f)-(1 - q)*d_1^2, 
-              ifelse( y-f <0,(1 - q)*(y - f)^2, 
-                      ifelse(y - f <= d_1, q*(y - f)^2, 2*q*d_1*(y - f)-q*d_1^2)))},
-    ngradient = function(y, f, w = rep(1, length(y))){
-      d_1 <- quantile(abs(y-f)[rep(1:length(y), w)], probs = tau)
-      ifelse( y - f< -d_1, -2*(1 - q)*d_1, ifelse(y - f < 0, 2*(1 - q)*(y - f), ifelse(y - f <= d_1,2*q*(y - f),2*q*d_1)))},
-    offset = function(y,w=rep(1, length(y))){
-      median(y[rep(1:length(y), w)])}
-  )
-}
-
+###############################################################################
 # translation of tau, k, pi:
 
 # translation: k to pi
@@ -243,7 +129,7 @@ Simfunc <- function(id,p=10,n=200){
     # Huber default:
     set.seed(i*1000+150000+id)
     tau_huber <- 0.8213748 # default huber 
-    boost_Huber             <- glmboost(y~.,family=M_Quant_Huber(tau=tau_huber),control = boost_control(mstop=1),data=datasimulation)
+    boost_Huber             <- glmboost(y~.,family=AdaptHuber(tau=tau_huber),control = boost_control(mstop=1),data=datasimulation)
     cvr_boost_Huber         <- cvrisk(boost_Huber,grid = 1:m_stop,papply="lapply")
     if(mstop(cvr_boost_Huber)>m_stop-50) cvr_boost_Huber <- cvrisk(boost_Huber,grid = 1:8000,papply="lapply")
     
@@ -257,7 +143,7 @@ Simfunc <- function(id,p=10,n=200){
     # Huber k=1
     set.seed(i*1000+150000+id)
     tau_huber <-tau2[2]
-    boost_Huber             <- glmboost(y~.,family=M_Quant_Huber(tau=tau_huber),control = boost_control(mstop=1),data=datasimulation)
+    boost_Huber             <- glmboost(y~.,family=AdaptHuber(tau=tau_huber),control = boost_control(mstop=1),data=datasimulation)
     cvr_boost_Huber         <- cvrisk(boost_Huber,grid = 1:m_stop,papply="lapply")
     if(mstop(cvr_boost_Huber)>m_stop-50) cvr_boost_Huber <- cvrisk(boost_Huber,grid = 1:8000,papply="lapply")
     
@@ -271,7 +157,7 @@ Simfunc <- function(id,p=10,n=200){
     # Huber k=2
     set.seed(i*1000+150000+id)
     tau_huber <-tau2[3]    
-    boost_Huber             <- glmboost(y~.,family=M_Quant_Huber(tau=tau_huber),control = boost_control(mstop=1),data=datasimulation)
+    boost_Huber             <- glmboost(y~.,family=AdaptHuber(tau=tau_huber),control = boost_control(mstop=1),data=datasimulation)
     cvr_boost_Huber         <- cvrisk(boost_Huber,grid = 1:m_stop,papply="lapply")
     if(mstop(cvr_boost_Huber)>m_stop-50) cvr_boost_Huber <- cvrisk(boost_Huber,grid = 1:8000,papply="lapply")
     
@@ -288,7 +174,7 @@ Simfunc <- function(id,p=10,n=200){
     set.seed(i*1000+250000+id)
     tau_bisquare <- 0.99
     
-    boost_bisquare        <- glmboost(y~.,family=M_Bisquare(tau=tau_bisquare),control = boost_control(mstop=1),data=datasimulation)
+    boost_bisquare        <- glmboost(y~.,family=AdaptBisquare(tau=tau_bisquare),control = boost_control(mstop=1),data=datasimulation)
     cvr_boost_bisquare     <- cvrisk(boost_bisquare,grid = 1:m_stop,papply="lapply")
     if(mstop(cvr_boost_bisquare)>m_stop-50) cvr_boost_bisquare <- cvrisk(boost_bisquare,grid = 1:8000,papply="lapply")
     
@@ -302,7 +188,7 @@ Simfunc <- function(id,p=10,n=200){
     ##############################
     set.seed(i*1000+250000+id)
     tau_bisquare <- 0.95
-    boost_bisquare        <- glmboost(y~.,family=M_Bisquare(tau=tau_bisquare),control = boost_control(mstop=1),data=datasimulation)
+    boost_bisquare        <- glmboost(y~.,family=AdaptBisquare(tau=tau_bisquare),control = boost_control(mstop=1),data=datasimulation)
     cvr_boost_bisquare     <- cvrisk(boost_bisquare,grid = 1:m_stop,papply="lapply")
     if(mstop(cvr_boost_bisquare)>m_stop-50) cvr_boost_bisquare <- cvrisk(boost_bisquare,grid = 1:8000,papply="lapply")
     
@@ -316,7 +202,7 @@ Simfunc <- function(id,p=10,n=200){
     ###############################
     set.seed(i*1000+250000+id)
     tau_bisquare <- 0.9
-    boost_bisquare        <- glmboost(y~.,family=M_Bisquare(tau=tau_bisquare),control = boost_control(mstop=1),data=datasimulation)
+    boost_bisquare        <- glmboost(y~.,family=AdaptBisquare(tau=tau_bisquare),control = boost_control(mstop=1),data=datasimulation)
     cvr_boost_bisquare     <- cvrisk(boost_bisquare,grid = 1:m_stop,papply="lapply")
     if(mstop(cvr_boost_bisquare)>m_stop-50) cvr_boost_bisquare <- cvrisk(boost_bisquare,grid = 1:8000,papply="lapply")
     
